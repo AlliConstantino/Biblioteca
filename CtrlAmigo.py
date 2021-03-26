b302b570f40ac2d75ebf5b09bf781e7512ccfbc1
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-03-25 23:03:03 -0300

    Update CtrlAmigo.py

--- CtrlAmigo.py
+++ CtrlAmigo.py
@@ -1,1 +1,12 @@
+class CtrlAmigo:
+  def __init(self,nome):
+    self.__nome = nome
+
+  @property
+  def nome(self):
+    return self.__nome
+
+  @nome.setter
+  def nome(self, nome):
+    self.__nome = nome  
 
