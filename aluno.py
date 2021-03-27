3013f463ff1a8dde42b115dbb3b15e49b0472d94
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-03-27 19:55:44 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,1 +1,5 @@
-class Aluno:
+from usuario import Usuario
+
+class Aluno(Usuario):
+    def __init__(self, nome, telefone, email, data_nascimento, ano_atual):
+        super().__init__(nome, telefone, email, data_nascimento, ano_atual)
