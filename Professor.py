2aa6476f53eebc73c241c9a39f49aa0d2d077876
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-03-27 19:53:40 -0300

    Update Professor.py

--- Professor.py
+++ Professor.py
@@ -1,1 +1,5 @@
-class Professor:
+from usuario import Usuario
+
+class Professor(Usuario):
+    def __init__(self, nome, telefone, email, data_nascimento, ano_atual):
+        super().__init__(nome, telefone, email, data_nascimento, ano_atual)
