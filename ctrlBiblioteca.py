efc2235122194ccac654f1e94b2912acbb5a1091
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-03-27 23:29:10 -0300

    Update ctrlBiblioteca.py

--- ctrlBiblioteca.py
+++ ctrlBiblioteca.py
@@ -1,1 +1,9 @@
+from editora import Editora
+from autor import Autor
+from capitulo import Capitulo
+from livro import Livro
+from usuario import Usuario
+from aluno import Aluno
+from professor import Professor
+
 class CtrlBiblioteca:
