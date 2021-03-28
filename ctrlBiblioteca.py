2bfa33935ff826e9e55f2fcdde359530882e64cb
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-03-27 23:33:24 -0300

    Update ctrlBiblioteca.py

--- ctrlBiblioteca.py
+++ ctrlBiblioteca.py
@@ -7,3 +7,11 @@ from aluno import Aluno
 from professor import Professor
 
 class CtrlBiblioteca:
+  def __init__(self):
+    #self.__controlador_livros = ControladorLivros(self)
+    #self.__controlador_amigos = ControladorAmigos(self)
+    #self.__controlador_emprestimos = ControladorEmprestimos(self)
+    #self.__tela_sistema = TelaSistema()
+  
+  def inicializa_sistema(self):
+    self.abre_tela()
