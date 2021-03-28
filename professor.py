5be1b37468b94bf381d9b0d68617611f10dfe9df
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-03-27 21:09:22 -0300

    Add files via upload

---
+++ professor.py
@@ -0,0 +1,5 @@
+from usuario import Usuario
+
+class Professor(Usuario):
+    def __init__(self, nome, telefone, email, data_nascimento, ano_atual):
+        super().__init__(nome, telefone, email, data_nascimento, ano_atual)
