353d3d51b96f064b08ef9edf046b5837cd3340fa
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-03-27 23:37:59 -0300

    Update ctrlBiblioteca.py

--- ctrlBiblioteca.py
+++ ctrlBiblioteca.py
@@ -5,10 +5,11 @@ from livro import Livro
 from usuario import Usuario
 from aluno import Aluno
 from professor import Professor
+from ctrlLivros import CtrlLivros
 
 class CtrlBiblioteca:
   def __init__(self):
-    #self.__controlador_livros = ControladorLivros(self)
+    self.__ctrlLivros = ctrlLivros(self)
     #self.__controlador_amigos = ControladorAmigos(self)
     #self.__controlador_emprestimos = ControladorEmprestimos(self)
     #self.__tela_sistema = TelaSistema()
