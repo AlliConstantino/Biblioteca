0974c318e5882bd39150fff6e84405e371e0f439
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-03-29 14:31:22 -0300

    Update telaBiblioteca.py

--- telaBiblioteca.py
+++ telaBiblioteca.py
@@ -1,1 +1,11 @@
-
+class TelaBiblioteca:
+  
+  def tela_opcoes(self):
+    print("-------- BIBLIOTECA ---------")
+    print("Escolha sua opcao")
+    print("1 - Livros")
+    print("2 - Usuários")
+    print("3 - Emprestimos")
+    print("0 - Finalizar sistema")
+    opcao = int(input("Escolha a opcao:"))
+    return opcao
