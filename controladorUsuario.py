9331516a549beb2516f8f96b39d9413a915a261f
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-03-31 19:28:19 -0300

    Update controladorUsuario.py

--- controladorUsuario.py
+++ controladorUsuario.py
@@ -8,91 +8,96 @@ from aluno import Aluno
 from professor import Professor
 
 
-def __init__(self, ctrlBiblioteca):
-    #super().__init__(nome, telefone, email, data_nascimento, ano_atual)
-    self.__aluno = []
-    self.__professor = []
+class ControladorUsuario():
 
-    self.__telaUsuario = TelaUsuario()
-    self.__ctrlBiblioteca = ctrlBiblioteca
+    def __init__(self, ctrlBiblioteca):
+        # super().__init__(nome, telefone, email, data_nascimento, ano_atual)
+        self.__aluno = []
+        self.__professor = []
 
+        self.__telaUsuario = TelaUsuario()
+        self.__ctrlBiblioteca = ctrlBiblioteca
 
-# Nao esquecer de lidar com as excecoes
+    # Nao esquecer de lidar com as excecoes
 
-# Aluno
-def incluir_aluno(self):
-    dados_aluno = self.__telaUsuario.pega_dados_aluno()
+    # Aluno
+    def incluir_aluno(self):
+        dados_aluno = self.__telaUsuario.pega_dados_aluno()
 
-    aluno = Aluno(dados_aluno["Nome"], dados_aluno["Telefone"], dados_aluno['Email'], dados_aluno['Data de nascimento'],
-                  dados_aluno['Ano atual'])
+        aluno = Aluno(dados_aluno["Nome"], dados_aluno["Telefone"], dados_aluno['Email'],
+                      dados_aluno['Data de nascimento'],
+                      dados_aluno['Ano atual'])
 
-    self.__aluno.append(aluno)
+        self.__aluno.append(aluno)
 
+    def lista_alunos(self):
+        for aluno in self.__aluno:
+            self.__telaUsuario.mostra_aluno({"Nome": aluno.nome, "Telefone": aluno.telefone, 'Email': aluno.email,
+                                             'Data de nascimento': aluno.data_nascimento, 'Ano atual': aluno.ano_atual})
 
-def lista_alunos(self):
-    for aluno in self.__aluno:
-        self.__telaUsuario.mostra_aluno({"Nome": aluno.nome, "Telefone": aluno.telefone, 'Email': aluno.email,
-                                         'Data de nascimento': aluno.data_nascimento, 'Ano atual': aluno.ano_atual})
+    def altera_aluno(self):
+        dados_aluno = self.__telaUsuario.pega_dados_aluno()
 
-def altera_aluno(self):
-    dados_aluno = self.__telaUsuario.pega_dados_aluno()
+        aluno = Aluno(dados_aluno["Nome"], dados_aluno["Telefone"], dados_aluno['Email'],
+                      dados_aluno['Data de nascimento'],
+                      dados_aluno['Ano atual'])
 
-    aluno = Aluno(dados_aluno["Nome"], dados_aluno["Telefone"], dados_aluno['Email'], dados_aluno['Data de nascimento'],
-                  dados_aluno['Ano atual'])
+        self.__aluno.append(aluno)
 
-    self.__aluno.append(aluno)
+    '''
+        def retornaAluno(self, aluno):
+            for aluno in self.__listaAlunos:
+                if (usuario.aluno = aluno):
+                    print(aluno.nome)
+                    print(aluno.email)
+                    print(aluno.emprestimos)
+    '''
 
-    def retornaAluno(self, aluno):
-        for aluno in self.__listaAlunos:
-            if (usuario.aluno = aluno):
-                print(aluno.nome)
-                print(aluno.email)
-                print(aluno.emprestimos)
+    def exclui_aluno(self):
+        if isinstance(aluno, Aluno):
+            aluno_incluso = False
+            for i in self.__aluno:
+                if i.nome == aluno.nome:
+                    self.__aluno.remove(i)
+                    aluno_incluso = True
+            if not aluno_incluso:
+                print('O aluno não estava incluso')
+        else:
+            print('Aluno inválido')
 
-def exclui_aluno(self):
-    if isinstance(aluno, Aluno):
-        aluno_incluso = False
-        for i in self.__aluno:
-            if i.nome == aluno.nome:
-                self.__aluno.remove(i)
-                aluno_incluso = True
-        if not aluno_incluso:
-            print('O aluno não estava incluso')
-    else:
-        print('Aluno inválido')
+    # Professor
 
+    def incluir_professor(self):
+        dados_professor = self.__telaUsuario.pega_dados_professor()
 
-# Professor
+        professor = Professor(dados_professor["Nome"], dados_professor["Telefone"], dados_professor['Email'],
+                              dados_professor['Data de nascimento'],
+                              dados_professor['Ano atual'])
 
-def incluir_professor(self):
-    dados_professor = self.__telaUsuario.pega_dados_professor()
+        self.__professor.append(professor)
 
-    professor = Professor(dados_professor["Nome"], dados_professor["Telefone"], dados_professor['Email'], dados_professor['Data de nascimento'],
-                  dados_professor['Ano atual'])
+    def lista_professores(self):
+        for professor in self.__professor:
+            self.__telaUsuario.mostra_professor(
+                {"Nome": professor.nome, "Telefone": professor.telefone, 'Email': professor.email,
+                 'Data de nascimento': professor.data_nascimento, 'Ano atual': professor.ano_atual})
 
-    self.__professor.append(professor)
+    def exclui_professor(self):
+        if isinstance(professor, Professor):
+            professor_incluso = False
+            for i in self.__professor:
+                if i.nome == professor.nome:
+                    self.__professor.remove(i)
+                    professor_incluso = True
+            if not professor_incluso:
+                print('O professor não estava incluso')
+        else:
+            print('professor inválido')
 
-def lista_professores(self):
-    for professor in self.__professor:
-        self.__telaUsuario.mostra_professor({"Nome": professor.nome, "Telefone": professor.telefone, 'Email': professor.email,
-                                         'Data de nascimento': professor.data_nascimento, 'Ano atual': professor.ano_atual})
-        
-def exclui_professor(self):
-    if isinstance(professor, professor):
-        professor_incluso = False
-        for i in self.__professor:
-            if i.nome == professor.nome:
-                self.__professor.remove(i)
-                professor_incluso = True
-        if not professor_incluso:
-            print('O professor não estava incluso')
-    else:
-        print('professor inválido')
+    def abre_tela(self):
+        # Atenção: código incompleto: adicionar funcões para todas as opções da tela
+        lista_opcoes = {1: self.incluir_aluno, 2: self.alterar_aluno, 3: self.lista_alunos, 4: self.excluir_aluno}
 
-def abre_tela(self):
-    # Atenção: código incompleto: adicionar funcões para todas as opções da tela
-    lista_opcoes = {1: self.incluir_aluno, 2: self.alterar_aluno,  3: self.lista_alunos, 4: self.excluir_aluno}
-
-    continua = True
-    while continua:
-        lista_opcoes[self.__tela_alunos.tela_opcoes()]()  # não entendi essa parte
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_alunos.tela_opcoes()]()  # não entendi essa parte
