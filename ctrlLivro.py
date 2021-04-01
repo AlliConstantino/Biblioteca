29fd80739574ced508362cf5d5059a5d54023326
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-03-31 22:38:13 -0300

    Update ctrlLivro.py
    Metodo checaStatus add.assim como metodo editaLivro

--- ctrlLivro.py
+++ ctrlLivro.py
@@ -13,3 +13,14 @@ class CtrlLivro:
             return livro
     return False  
   
+  def editaLivro(self,titulo,autor,genero,capitulo):
+    #metodo para editar o livro
+    return False
+  
+  #Metodo para checar se livro esta emprestado ou nao
+  def checaStatus(self,livro):
+    #Pergunta pro usuario qual livro ele quer checar
+    livroAChecar = input("Qual livro você quer checar se esta disponivel?")
+    #Pega o input e percorre o array dos livros emprestados,comparando se o livro buscado está la
+    #Se estiver retorna uma msg dizendo que o livro esta emprestado e portanto nao esta disponivel
+    #Se nao estiver retorna uma msg dizendo que esta disponivel e encerra
