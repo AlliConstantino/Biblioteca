c6ec1380c165ffcecee1e9a97e6623f393572913
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-04-02 18:12:25 -0300

    Update capitulo.py

--- capitulo.py
+++ capitulo.py
@@ -1,7 +1,24 @@
 #importacao do Livro
+from livro import Livro
 #importacao do ctrlLivro
+from ctrlLivro import CtrlLivro
 
 class Capitulo:
   #init
-  #getter
-  #setter
+  def __init__(self, numeroCap: int, nomeCap: str):
+        self.__numeroCap = numeroCap
+        self.__nomeCap = nomeCap
+
+  #getter numeroCap
+  @property
+  def mostraNumeroCap(self):
+    return self.__numeroCap
+  
+  #getter nomeCap
+  @property
+  def mostraNomeCap(self):
+    return self.__nomeCap
+  
+  #getter cap
+  def mostraCap(self,nomeCap,numeroCap):
+    print(nomeCap, numeroCap)
