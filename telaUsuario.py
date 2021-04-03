d884056fb7aedd622e00b5ab83a8444a9d520e6e
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-04-03 00:03:57 -0300

    Update telaUsuario.py

--- telaUsuario.py
+++ telaUsuario.py
@@ -3,39 +3,43 @@ import datetime
 
 class TelaUsuario():
 
-  def tela_opcoes(self):
-    print("-------- USUÁRIOS ----------")
-    print("1 - Incluir aluno")
-    print("2 - Alterar aluno")
-    print("3 - Listar alunos")
-    print("4 - Excluir aluno")
-    print("0 - Retornar")
+    def tela_opcoes(self):
+        print("-------- USUÁRIOS ----------")
+        print("1 - Incluir aluno")
+        print("2 - Alterar aluno")
+        print("3 - Listar alunos")
+        print("4 - Excluir aluno")
+        print("5 - Incluir professor")
+        print("6 - Alterar professor")
+        print("7 - Listar professor")
+        print("8 - Excluir professor")
+        print("0 - Retornar")
 
-    #Temos que rever como vamos fazer essas opções ao add professor
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
 
-    opcao = int(input("Escolha a opcao: "))
-    return opcao
+    def pega_dados_usuario(self):
+        telefone = input("Telefone: ")
+        email = input("E-mail: ")
+        data_entrada = input("Data de Nacimento (no formato DD/MM/AA: ")
+        dia, mes, ano = map(int, data_entrada.split('/'))
+        data_nascimento = datetime.date(dia, mes, ano)
+        ano_atual = input("Ano atual: ")
 
-  def pega_dados_aluno(self):
-    print("-------- INCLUIR ALUNO ----------")
-    nome = input("Nome: ")
-    telefone = input("Telefone: ")
-    email = input("E-mail: ")
-    data_entrada = input("Data de Nacimento (no formato DD/MM/AA: ")
-    dia, mes, ano = map(int, data_entrada.split('/'))
-    data_nascimento = datetime.date(dia, mes, ano)
-    ano_atual = input("Ano atual: ")
+        return {"Telefone": telefone, 'Email': email,
+                'Data de nascimento': data_nascimento, 'Ano atual': ano_atual}
 
-    return {"Nome": aluno.nome, "Telefone": aluno.telefone, 'Email': aluno.email, 'Data de nascimento': aluno.data_nascimento, 'Ano atual': aluno.ano_atual}
+        # nome ser str; telefone int; dtaa no formato solicitado, e-mail com @.
 
-  def mostra_aluno(self, dados_aluno):
-    print("NOME DO ALUNO: ", dados_aluno["Nome"])
-    print("FONE DO ALUNO: ", dados_aluno["Telefone"])
-    print("E-MAIL DO ALUNO: ", dados_aluno['Email'])
-    print("NASCIMENTO DO ALUNO: ", dados_aluno['Data de nascimento'])
-    print("ANO ATUAl: ",dados_aluno['Ano atual'])
+    def pega_nome(self, tipo: str):
+        usuario = tipo.upper()
+        print("-------- %s ---------" % usuario)
+        nome = input("Nome: ")
+        return {"Nome": nome}
 
-    #ADICIONAR demais métodos
-
-
-#Adicionar Professor
+    def mostra_usuario(self, dados_usuario, tipo):
+        print("NOME DO %s: %s" % (tipo, dados_usuario["Nome"]))
+        print("FONE DO %s: %s" % (tipo, dados_usuario["Telefone"]))
+        print("E-MAIL DO %s: %s" % (tipo, dados_usuario['Email']))
+        print("NASCIMENTO DO %s: %x" % (tipo, dados_usuario['Data de nascimento']))
+        print("ANO ATUAl: %d" % (dados_usuario['Ano atual']))
