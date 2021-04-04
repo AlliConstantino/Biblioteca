8a761c306b213749a0b0b8f18d9e9694ed1ec41c
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-04-04 18:00:26 -0300

    Update ctrlLivro.py

--- ctrlLivro.py
+++ ctrlLivro.py
@@ -46,9 +46,15 @@ class CtrlLivro:
 
       # Metodo para checar se livro esta emprestado ou nao
 
+      #tá certo sera?
   def checaStatus(self, livro):
       # Pergunta pro usuario qual livro ele quer checar
       livroAChecar = input("Qual livro você quer checar se esta disponivel?")
       # Pega o input e percorre o array dos livros emprestados,comparando se o livro buscado está la
+      for i in self.__livro:
+        if self.livro.status() == "emprestado":
+          print ("livro já foi emprestado)
+        else:
+          print("livro disponivel")       
       # Se estiver retorna uma msg dizendo que o livro esta emprestado e portanto nao esta disponivel
       # Se nao estiver retorna uma msg dizendo que esta disponivel e encerra
