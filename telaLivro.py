f2566031458357983c4f98d915e2b55bb69f0eea
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-04-04 16:41:11 -0300

    Update telaLivro.py

--- telaLivro.py
+++ telaLivro.py
@@ -1,7 +1,28 @@
+from autor import Autor
+from livro import Livro
+from ctrlLivro import CtrlLivro
+from ctrlBiblioteca import CtrlBiblioteca
+
 class TelaLivro:
   
   def mostraLivro(self,titulo,capitulo,autor):
+    #pegar o metodo mostra Livro do ctrlBiblioteca
     return False
   
-  #demais metodos...
+  #exclui livro
+  def excluirLivro(self,titulo,capitulo,autor):
+    #pegar o metodo excluir Livro do ctrlBiblioteca
+    return False
+  
+  #mostra nome do livro
+  def telaOpcoes(self):
+    print("-------- LIVROS ----------")
+        print("1 - Incluir livro")
+        print("2 - Alterar livro")
+        print("3 - Mostrar livro")
+        print("0 - Retornar")
+
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
+  #
   
