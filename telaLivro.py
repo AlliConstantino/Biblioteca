eaf4d573023443ab84a83520fe0f3cd4e5d8f3c2
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2021-04-05 05:07:37 -0300

    Update telaLivro.py

--- telaLivro.py
+++ telaLivro.py
@@ -11,28 +11,20 @@ class TelaLivro():
         opcao = int(input("Escolha a opcao: "))
         return opcao
 
-    # mostra o livro
-    def mostraLivro(self, titulo, capitulo, autor):
-        return self.ctrlLivro.retornaLivro(titulo)
-
-    # exclui livro
-    def excluirLivro(self, titulo, capitulo, autor):
-        # pegar o metodo excluir Livro do ctrlBiblioteca
-        return self.ctrBiblioteca.exclui_livro()
-
-    def incluirLivro(self, titulo, capitulo, autor):
-        return self.ctrlBiblioteca.inclui_livro(self,titulo,capitulo,autor)
+    
         
-    def pega_nome(self, tipo: str):
-        continua = True
-        while continua:
-            titulo = tipo.upper()
-            print("-------- %s ---------" % titulo)
-            titulo = input("Titulo: ")
-            continua = any(char.isdigit() for char in titulo)
-            if continua:
-                print('Digite um nome válido')
+    def pega_titulo(self,titulo):
+        titulo = input("Titulo: ")
+        #autor = input("Autor: ")
 
         return {"Titulo": titulo}
+        
+    def pega_autor(self,autor):
+        autor = input("Autor: ")
+        
+        return{"Autor", autor}
+        
+    def pega_dados_livro():
+        return False
 
 
